8463093a9da8d50d6c1358b523a9a81151efd78a
Sortcoder <sortcoder.info@gmail.com>
2023-05-04 10:18:16 +0530

    first commit

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -2,21 +2,26 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "./curve-contracts/ISwap.sol";
 import "./uniswapv2-contracts/ISwapRouterV2.sol";
 import "./uniswapv3-contracts/ISwapRouter.sol";
 import "./curve-contracts/IProvider.sol";
+import "./balancer-contracts/IBalancerVault.sol";
+import "./balancer-contracts/IFlashLoanRecipient.sol";
 
-contract FlashLoanArbitrage {
-    address addressProviderCurve;
-    address owner;
+contract FlashLoanArbitrage is IFlashLoanRecipient {
+    using SafeMath for uint256;
 
-    constructor(
-        address _addressProviderCurve
-    ) {
+    address public immutable vault;
+    address public addressProviderCurve;
+    address public owner;
+
+    constructor(address _addressProviderCurve, address _vault) {
         addressProviderCurve = _addressProviderCurve;
         owner = msg.sender;
+        vault = _vault;
     }
 
     struct Trade {
@@ -25,7 +30,7 @@ contract FlashLoanArbitrage {
         address _tokenOut;
         uint amount;
         address pool;
-        uint poolFee;
+        uint24 poolFee;
         string exchange;
     }
 
@@ -62,7 +67,7 @@ contract FlashLoanArbitrage {
         uint256[] memory amountOut = ISwapRouterV2(router)
             .swapExactTokensForTokens(
                 _amount,
-                minAmount, 
+                minAmount,
                 path,
                 address(this),
                 deadline
@@ -94,12 +99,10 @@ contract FlashLoanArbitrage {
         uint256 amountOut = ISwapRouter(router).exactInputSingle(params);
         return amountOut;
     }
-    
-    function returnsExchangeAddress(uint256 _id)
-        internal
-        view
-        returns (address)
-    {
+
+    function returnsExchangeAddress(
+        uint256 _id
+    ) internal view returns (address) {
         return IProvider(addressProviderCurve).get_address(_id);
     }
 
@@ -107,41 +110,150 @@ contract FlashLoanArbitrage {
         address _pool,
         address _from,
         address _to,
-        uint256 _amount,
-        uint256 _expected,
-        address _receiver
+        uint256 _amount
     ) internal {
         address exchangeContract = returnsExchangeAddress(2);
         IERC20(_from).approve(exchangeContract, _amount);
+
+        uint expected = ISwap(exchangeContract).get_exchange_amount(
+            _pool,
+            _from,
+            _to,
+            _amount
+        );
 
         ISwap(exchangeContract).exchange(
             _pool,
             _from,
             _to,
             _amount,
-            _expected,
-            _receiver
+            expected,
+            address(this)
         );
     }
 
-    // function executeTrade(
-    //     Trade[] memory trades
-    // ) public {
-    //     for(uint i=0; i < trades.length; i++){
-    //         Trade memory trade = trades[i];
-    //         if (keccak256(abi.encodePacked(trade.exchange)) == keccak256(abi.encodePacked("UNISWAPV2"))){
-    //             swapTokenOnUnsiwapv2(trade._router, trade._tokenIn, trade._tokenOut, trade.amount);
-    //         }       
-    //     }
-    // }
-}
-    // struct Trade {
-    //     address _router;
-    //     address _tokenIn;
-    //     address _tokenOut;
-    //     uint amount;
-    //     address pool;
-    //     uint poolFee;
-    //     string exchange;
-    // }
+    function executeTrade(Trade[] memory trades) public {
+        uint tradeAmount = trades[0].amount;
+        uint tokenInInitialBalance = IERC20(trades[0]._tokenIn).balanceOf(
+            address(this)
+        );
 
+        for (uint i = 0; i < trades.length; i++) {
+            Trade memory trade = trades[i];
+
+            uint tokenOutInitialBalance = IERC20(trades[i]._tokenOut).balanceOf(
+                address(this)
+            );
+
+            if (
+                keccak256(abi.encodePacked(trade.exchange)) ==
+                keccak256(abi.encodePacked("UNISWAPV2"))
+            ) {
+                swapTokenOnUnsiwapv2(
+                    trade._router,
+                    trade._tokenIn,
+                    trade._tokenOut,
+                    tradeAmount
+                );
+            }
+            if (
+                keccak256(abi.encodePacked(trade.exchange)) ==
+                keccak256(abi.encodePacked("SUSHISWAP"))
+            ) {
+                swapTokenOnUnsiwapv2(
+                    trade._router,
+                    trade._tokenIn,
+                    trade._tokenOut,
+                    tradeAmount
+                );
+            }
+            if (
+                keccak256(abi.encodePacked(trade.exchange)) ==
+                keccak256(abi.encodePacked("UNISWAPV3"))
+            ) {
+                swapTokenOnUniswapv3(
+                    trade._tokenIn,
+                    trade._tokenOut,
+                    tradeAmount,
+                    trade.poolFee,
+                    trade._router
+                );
+            }
+            if (
+                keccak256(abi.encodePacked(trade.exchange)) ==
+                keccak256(abi.encodePacked("CURVE"))
+            ) {
+                exchangesTokensOnCurve(
+                    trade.pool,
+                    trade._tokenIn,
+                    trade._tokenOut,
+                    tradeAmount
+                );
+            }
+
+            uint tokenOutBalance = IERC20(trades[i]._tokenOut).balanceOf(
+                address(this)
+            );
+            tradeAmount = tokenOutBalance - tokenOutInitialBalance;
+        }
+
+        uint tokenInFinalBalance = IERC20(trades[0]._tokenIn).balanceOf(
+            address(this)
+        );
+        // require(tokenInFinalBalance > tokenInInitialBalance, "Trade Not Profitable")
+    }
+
+    function receiveFlashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory userData
+    ) external override {
+        for (uint256 i = 0; i < tokens.length; ++i) {
+            IERC20 token = tokens[i];
+            uint256 amount = amounts[i];
+            uint256 feeAmount = feeAmounts[i];
+
+            (Trade[] memory trades) = abi.decode(userData, (Trade[]));
+            executeTrade(trades);
+
+            // Return loan
+            token.transfer(vault, amount);
+        }
+    }
+
+    function flashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        bytes memory userData
+    ) external {
+        IBalancerVault(vault).flashLoan(
+            IFlashLoanRecipient(address(this)),
+            tokens,
+            amounts,
+            userData
+        );
+    }
+
+    function withdraw(address _tokenAddress) external {
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+
+    function getBalance(
+        address _tokenContractAddress
+    ) external view returns (uint256) {
+        uint balance = IERC20(_tokenContractAddress).balanceOf(address(this));
+        return balance;
+    }
+
+}
+// struct Trade {
+//     address _router;
+//     address _tokenIn;
+//     address _tokenOut;
+//     uint amount;
+//     address pool;
+//     uint poolFee;
+//     string exchange;
+// }
